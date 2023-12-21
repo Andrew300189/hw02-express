fb923a06178539bdf8bec60dff7eb5ffa8a6b20e
Andrey <94688521+Andrew300189@users.noreply.github.com>
2023-12-21 22:37:04 +0200

    Changes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,65 @@
-const express = require('express')
-
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require('../../models/contacts');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const contacts = await listContacts();
+  res.json(contacts);
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  if (contact) {
+    res.json(contact);
+  } else {
+    res.status(404).json({ message: 'Not found' });
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { body } = req;
+  try {
+    const newContact = await addContact(body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { contactId } = req.params;
+  const result = await removeContact(contactId);
+
+  if (result) {
+    res.json({ message: 'contact deleted' });
+  } else {
+    res.status(404).json({ message: 'Not found' });
+  }
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { contactId } = req.params;
+  const { body } = req;
 
-module.exports = router
+  try {
+    const updatedContact = await updateContact(contactId, body);
+
+    if (updatedContact) {
+      res.json(updatedContact);
+    } else {
+      res.status(404).json({ message: 'Not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
