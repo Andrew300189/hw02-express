38e7564ec506f98e5c64f884fa5c4e21499158c6
Andrey <94688521+Andrew300189@users.noreply.github.com>
2024-01-01 20:59:38 +0200

    First commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,11 +1,23 @@
-const express = require('express');
-const router = express.Router();
-const controllers = require('../../controllers');
+const express = require("express");
 
-router.get('/', controllers.contacts.getContacts);
-router.get('/:contactId', controllers.contacts.getContact);
-router.post('/', controllers.contacts.createContact);
-router.delete('/:contactId', controllers.contacts.deleteContact);
-router.put('/:contactId', controllers.contacts.updateContactInfo);
+const ctrl = require("../../controllers/contacts");
+
+const {validateBody, isValidId} = require("../../middlewares");
+
+const {schemas} = require("../../models/contact");
+
+const router = express.Router();
+
+router.get("/", ctrl.getAll);
+
+router.get("/:id", isValidId, ctrl.getById);
+
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
+
+router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+
+router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
+
+router.delete("/:id", isValidId, ctrl.deleteById);
 
 module.exports = router;
