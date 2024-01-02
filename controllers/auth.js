94927e03857fd7c4779a0372a30f208799cf246d
Andrey <94688521+Andrew300189@users.noreply.github.com>
2024-01-02 21:09:12 +0200

    Changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,7 @@ const register = async(req, res)=> {
     const user = await User.findOne({email});
 
     if(user){
-        throw HttpError(409, "Email already in use");
+        throw HttpError(409, "Email in use");
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
@@ -29,18 +29,18 @@ const login = async(req, res)=> {
     const {email, password} = req.body;
     const user = await User.findOne({email});
     if(!user){
-        throw HttpError(401, "Email or password invalid");
+        throw HttpError(401, "Email or password is wrong");
     }
     const passwordCompare = await bcrypt.compare(password, user.password);
     if(!passwordCompare) {
-        throw HttpError(401, "Email or password invalid");
+        throw HttpError(401, "Email or password is wrong");
     }
 
     const payload = {
         id: user._id,
     }
 
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "1000h"});
     await User.findByIdAndUpdate(user._id, {token});
 
     res.json({
--- models/user.js
+++ models/user.js
@@ -3,27 +3,26 @@ const Joi = require("joi");
 
 const {handleMongooseError} = require("../helpers");
 
-const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        match: emailRegexp,
-        unique: true,
-        required: true,
-    },
     password: {
         type: String,
-        minlength: 6,
-        required: true,
-    },
-    token: {
+        required: [true, 'Password is required'],
+      },
+      email: {
         type: String,
-        default: ""
+        required: [true, 'Email is required'],
+        unique: true,
+      },
+      subscription: {
+        type: String,
+        enum: ["starter", "pro", "business"],
+        default: "starter"
+      },
+      token: {
+        type: String,
+        default: null,
     }
 }, {versionKey: false, timestamps: true});
 
@@ -33,7 +32,7 @@ const registerSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().pattern(emailRegexp).required(),
     password: Joi.string().min(6).required(),
-})
+});
 
 const loginSchema = Joi.object({
     email: Joi.string().pattern(emailRegexp).required(),
