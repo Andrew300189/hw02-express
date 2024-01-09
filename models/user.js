6886ba1ff1fb9b34f1d758ed69cdc58b01da458d
Andrey <94688521+Andrew300189@users.noreply.github.com>
2024-01-09 23:19:16 +0200

    Changes

--- models/user.js
+++ models/user.js
@@ -29,7 +29,16 @@ const userSchema = new Schema({
       type: String,
       minlength: 6,
       required: true,
-  }
+  },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, 'Verify token is required'],
+    },
+  
 }, {versionKey: false, timestamps: true});
 
 userSchema.post("save", handleMongooseError);
@@ -47,6 +56,10 @@ const registerSchema = Joi.object({
     password: Joi.string().min(6).required(),
 });
 
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+})
+
 const loginSchema = Joi.object({
     email: Joi.string().pattern(emailRegexp).required(),
     password: Joi.string().min(6).required(),
@@ -55,6 +68,7 @@ const loginSchema = Joi.object({
 const schemas = {
     registerSchema,
     loginSchema,
+    emailSchema,
 }
 
 const User = model("user", userSchema);
